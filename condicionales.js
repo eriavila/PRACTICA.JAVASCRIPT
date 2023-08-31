69b9f2b26aa878da15bf0772b03af706a2c6bf1e
Valeria Avila <139603959+eriavila@users.noreply.github.com>
2023-08-30 22:47:42 -0500

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,32 @@
+const nombre="Ada school"
+let edad= "123"
+let nota = 8
+
+if(nombre==="Ada school"&& nota >-7) {
+
+
+console.log("hola"+ nombre +"su nota ="+nota)
+}else{
+    console.log("El nombre no existe");
+} 
+
+// funciones 
+
+function areacircunferencia ( radio){
+
+const pi = 3.1415169
+const resultado = p1 * radio * radio;
+// console.log(resultado)
+return resultado;
+}
+
+
+
+const result = areacircunferencia(528);
+const result1 = areacircunferencia(12348)
+const result2 = areacircunferencia(28)
+const result3 = areacircunferencia(21)
+
+function TransformStreamDefaultController() {
+
+}
