b0f79e80dfe21f1f4ae945a86b611273d24ab6e6
Valeria Avila <139603959+eriavila@users.noreply.github.com>
2023-09-01 22:20:59 -0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,56 @@
+//Arrays
+
+let frutas = [];
+
+frutas[0]= "manzanas";
+frutas[1]= "bananas";
+frutas[2]="peras";
+frutas[3]="peras" ;
+frutas[4]="peras";
+frutas[3]= "frutillas";
+
+
+//frutas[3]="frutillas"
+
+frutas[1]="platanos";
+
+console.log(frutas);
+
+let frutas2 =["platanos","peras","frutillas","manzanas",]; 
+let numeros =[123,534,775.123]
+let valores = [false,true,false]
+
+//console.log(frutas2);
+
+let frutas3 = [
+"platanos",
+"peras",
+"frutillas",
+"manzanas",
+"platanos",
+"frutillas",
+"manzana45",
+
+];
+
+for (let i= 0;i<frutas3.length;i++) {
+    if (frutas3[i] === "platanos"){
+console.log("Se encontro la fruta")
+    }
+}
+
+
+
+
+
+
+
+
+let miFamilia = [];
+miFamilia [0] = "luisa";
+miFamilia [1] = "Nicolas";
+miFamilia [2] = "Valeria";
+miFamilia [3] = "Susana";
+
+
+
